c5df264c33082814706dd8748e8aaf4c9d0b438a
SPlinterRed <131634018+SPlinterRed@users.noreply.github.com>
2024-11-16 00:51:06 +0800

    sss

--- back.js
+++ back.js
@@ -4,3 +4,18 @@ const button2 = document.createElement('button');
 button2.id = 'sigma';
 button2.textContent = 'trybutton';
 sigma.append(button2);
+
+const button3 = document.createElement('button');
+button3.id = 'beta';
+button3.textContent = 'betabutton';
+sigma.append(button3);
+for( i = 0; i < 5; i ++){
+    print("what the hell");
+    if(i == 4){
+        i = 0;
+        button2.remove();
+        button2.append(sigma);
+        button3.remove();
+        button2.append(sigma)
+    }
+}
