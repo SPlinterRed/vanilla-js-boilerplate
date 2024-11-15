399f125b974bcb9030c87884bd9a96272dab728e
SPlinterRed <131634018+SPlinterRed@users.noreply.github.com>
2024-11-16 00:42:25 +0800

    sss

---
+++ back.js
@@ -0,0 +1,6 @@
+const sigma = document.getElementById('Backend');
+
+const button2 = document.createElement('button');
+button2.id = 'sigma';
+button2.textContent = 'trybutton';
+sigma.append(button2);
